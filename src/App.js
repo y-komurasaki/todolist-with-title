0a9bc46d345224543a7e2625ce30aa6ea2f13e93
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2023-03-06 08:44:26 +0900

    Redux設定

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { useSelector } from 'react-redux';
 
 
 function App() {
-  const  {title, content}  = useSelector((state) =>state.tasks);
+  const  {id, title, content}  = useSelector((state) =>state.tasks);
   return (
     <div className="App">
       <>
-        <h1>{title}</h1>
-        <h1>{content}</h1>
+        <div className="displayTask">
+          <h1>{title}</h1>
+          <h2>{content}</h2>
+        </div>
+        <div className="addTodo">
+          <input type="text" placeholder='Todoを入力'/>
+          <button>追加</button>
+        </div>
       </>
     </div>
   );
--- src/features/Tasks.js
+++ src/features/Tasks.js
@@ -1,17 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  title: 'test',//初期状態
+  id: 1,
+  title: 'Todo一覧',//初期状態
   content:'test',
 }
 const tasksSlice = createSlice({
   name: "tasks", //Slice自体の名前
   initialState, //初期状態
-  // reducers: { //アクション
-  //   addTask: (state, action) => { 
-  //     state.value.push(action.payload);
-  //   },
-  // }, 
+  reducers: { //アクション
+    addTask: (state, action) => { 
+      state.value.push(action.payload);
+    },
+  }, 
 });
 
 export default tasksSlice.reducer; //reducerをstore.jsに渡す
