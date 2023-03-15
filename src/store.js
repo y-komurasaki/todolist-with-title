23f5309a5d0062476fef9f7992cbddbfa3aae925
y-komurasaki <97991049+y-komurasaki@users.noreply.github.com>
2023-03-15 17:55:54 +0900

    Revert "FunctionFix"

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import tasksReducer from "./features/Tasks";
-// import titleReducer from "./features/Title";
-
-export const store = configureStore({
-  reducer: {
-     tasks: tasksReducer,
-  },
-});
