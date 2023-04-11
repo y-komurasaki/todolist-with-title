a563578f253f5bc0bc7b5bf52a83ef3744efd1ba
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2023-04-11 12:39:49 +0900

    DeleteTaskListComponentSplit

---
+++ src/components/taskLists/DeleteTaskList.js
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+
+const DeleteTaskList = ({ list, openModal }) => {
+  const deleteTaskListClick = () => {
+    //現在クリックしているタスクidを情報を受け取る
+    openModal(list.listId);
+  };
+
+  return (
+    <FontAwesomeIcon
+      className="listDeleteButton"
+      icon={faTrashCan}
+      onClick={() => deleteTaskListClick(list.listId)}
+    ></FontAwesomeIcon>
+  );
+};
+
+export default DeleteTaskList;
--- src/components/taskLists/TaskLists.js
+++ src/components/taskLists/TaskLists.js
@@ -1,26 +1,16 @@
 import { useSelector } from "react-redux";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import DeleteTaskList from "./DeleteTaskList";
 import EditTaskList from "./EditTaskList";
 
 const TaskLists = ({ openAddModal, openModal }) => {
   const tasks = useSelector((state) => state.tasks);
-
-  const deleteTaskListClick = (currentListId) => {
-    //現在クリックしているタスクidを情報を受け取る
-    openModal(currentListId);
-  };
 
   return (
     <div className="taskLists">
       {tasks.taskLists.map((list) => (
         <div key={list.listId} className="taskList">
           <EditTaskList list={list} openAddModal={openAddModal} />
-          <FontAwesomeIcon
-            className="listDeleteButton"
-            icon={faTrashCan}
-            onClick={() => deleteTaskListClick(list.listId)}
-          ></FontAwesomeIcon>
+          <DeleteTaskList list={list} openModal={openModal} />
         </div>
       ))}
     </div>
